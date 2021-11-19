f7583f6023290109aab0e85ba219c1059efcc48f
Taixi Lu <taixilu@taixis-air.lan>
2021-11-19 16:35:18 -0500

    add1

--- server.js
+++ server.js
@@ -2,15 +2,18 @@
 var express = require("express")
 var app = express()
 // Require database SCRIPT file
-
+var db = require("./database.js");
 // Require md5 MODULE
+var md5 = require("md5");
+// Require a middleware extension for express 
+var bodyParser = require("body-parser");
 
 // Make Express use its own built-in body parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set server port
-
+var HTTP_PORT = 5000;
 // Start server
 app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
@@ -25,19 +28,27 @@ app.get("/app/", (req, res, next) => {
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
 
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
-app.get("/app/users", (req, res) => {	
+app.get("/app/users/", (req, res) => {	
 	const stmt = db.prepare("SELECT * FROM userinfo").all();
 	res.status(200).json(stmt);
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
+app.get("/app/user/:id", (req, res) => {
 
+	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?").all(2);
+	res.status(200).json(stmt);
+
+});
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
-
+app.patch("/app/apdate/user/:id", (req, res) => {
+	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?").all();
+	res.status(200).json(stmt);
+});
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 
 // Default response for any other request
 app.use(function(req, res){
-	res.json({"message":"Endpoint not found. (404)"});
+	res.json({"message":"Your API is working!"});
     res.status(404);
 });
